9e22753aeacb1ac671b8a31db58e1bb55e53ba25
Robert Haskell <robertwhaskell@gmail.com>
2014-11-18 09:07:52 -0800

    initial commit

--- CodeFellowsUser/urls.py
+++ CodeFellowsUser/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^users/', include('cf_users.urls', namespace="cf_users")),
+    url(r'', include('cf_users.urls', namespace="cf_users")),
     url(r'^admin/', include(admin.site.urls)),
 )
